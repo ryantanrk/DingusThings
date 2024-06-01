e2c7e76ad2e6c4f4d894fbee8125e70c7d87b38f
ryantanrk <ryantanrk@gmail.com>
2024-06-01 19:47:12 +0800

    initial commit

---
+++ Behaviours/MyHeartPhysicsProp.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace DingusThings.Behaviours
+{
+    internal class MyHeartPhysicsProp : PhysicsProp
+    {
+        public override void ItemActivate(bool used, bool buttonDown = true)
+        {
+            base.ItemActivate(used, buttonDown);
+            if (buttonDown)
+            {
+                AssetBundle? bundle = DingusThings.Bundle;
+                if (bundle == null) 
+                {
+                    DingusThings.Logger.LogError("Sound failed to play.");
+                    return;
+                }
+                AudioClip audioClip = bundle.LoadAsset<AudioClip>("Assets/DingusThings/Sounds/SheSaid.ogg");
+                AudioSource audioSource = GetComponent<AudioSource>();
+                audioSource.PlayOneShot(audioClip, 1F);
+            }
+        }
+    }
+}
---
+++ DingusThings.cs
@@ -0,0 +1,75 @@
+using BepInEx;
+using BepInEx.Logging;
+using DingusThings.Behaviours;
+using HarmonyLib;
+using LethalLib.Modules;
+using System.IO;
+using System.Reflection;
+using UnityEngine;
+
+namespace DingusThings
+{
+    [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+    [BepInDependency(LethalLib.Plugin.ModGUID)]
+    public class DingusThings : BaseUnityPlugin
+    {
+        public static DingusThings Instance { get; private set; } = null!;
+        internal new static ManualLogSource Logger { get; private set; } = null!;
+        internal static Harmony? Harmony { get; set; }
+
+        public static AssetBundle? Bundle;
+
+        public static string PluginString = $"{MyPluginInfo.PLUGIN_GUID} v{MyPluginInfo.PLUGIN_VERSION}";
+
+        private void Awake()
+        {
+            Logger = base.Logger;
+            Instance = this;
+            string sAssemblyLocation = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+            // load assets
+            Bundle = AssetBundle.LoadFromFile(Path.Combine(sAssemblyLocation, "dingus"));
+            if (Bundle == null)
+            {
+                Logger.LogError($"{PluginString}: Failed to load custom assets.");
+                return;
+            }
+
+            int iRarity = 100;
+            Item myHeartItem = Bundle.LoadAsset<Item>("Assets/DingusThings/Items/MyHeart.asset");
+            // add custom behavior
+            MyHeartPhysicsProp myHeartProp = myHeartItem.spawnPrefab.AddComponent<MyHeartPhysicsProp>();
+            myHeartProp.grabbable = true;
+            myHeartProp.grabbableToEnemies = true;
+            myHeartProp.isInFactory = true;
+            myHeartProp.itemProperties = myHeartItem;
+
+            // register prefab
+            NetworkPrefabs.RegisterNetworkPrefab(myHeartItem.spawnPrefab);
+            Utilities.FixMixerGroups(myHeartItem.spawnPrefab);
+            Items.RegisterScrap(myHeartItem, iRarity, Levels.LevelTypes.All);
+
+            Logger.LogInfo($"{PluginString} has loaded!");
+        }
+
+        internal static void Patch()
+        {
+            Harmony ??= new Harmony(MyPluginInfo.PLUGIN_GUID);
+
+            Logger.LogDebug("Patching...");
+
+            Harmony.PatchAll();
+
+            Logger.LogDebug("Finished patching!");
+        }
+
+        internal static void Unpatch()
+        {
+            Logger.LogDebug("Unpatching...");
+
+            Harmony?.UnpatchSelf();
+
+            Logger.LogDebug("Finished unpatching!");
+        }
+    }
+}
